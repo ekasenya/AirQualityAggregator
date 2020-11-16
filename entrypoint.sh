a87ae6247b9ea8d5d6e42b62e640e1d29b6c321e
katty87 <esenenkova@gmail.com>
2020-11-16 22:40:44 +0300

    Add docker

---
+++ entrypoint.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+python3 manage.py flush --no-input
+python3 manage.py migrate --no-input
+python3 manage.py test --no-input
+if [[ $? -eq 0 ]]
+then
+    python3 manage.py runserver 0.0.0.0:80
+fi;
